bc701665461340cb5996c5ad652de4714a171273
Pinta365 <anders@billfors.com>
2023-09-30 17:07:34 +0200

    updates

--- src/loggers/denokv_logger.ts
+++ src/loggers/denokv_logger.ts
@@ -1,10 +1,15 @@
 import { Logger, LoggerData } from "../logger_manager.ts";
-const database = await Deno.openKv();
+let database: Deno.Kv | null = null; // Prevents the db from connecting when using other loggers.
 
-// Should parse and type the data better.
+// Should parse and type the data better than "LoggerData["payload"]"
 
 async function insertEvent(payload: LoggerData["payload"]) {
     try {
+        
+        if (!database) {
+            database = await Deno.openKv(Deno.env.get("DENO_KV_LOCAL_DATABASE") || undefined);
+        }
+
         await database.set([payload.timestamp, payload.realmId, payload.projectId, payload.type], payload); 
         // More indexes..      
     } catch (error) {
@@ -14,9 +19,8 @@ async function insertEvent(payload: LoggerData["payload"]) {
 
 export class denokvLogger implements Logger {
     async log(data: LoggerData): Promise<void> {
-
         const {payload} = data;
+        console.log(payload.type);
         await insertEvent(payload);
-
     }
 }
