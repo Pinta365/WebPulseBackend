601a4ca45effa27d2fdad22c2fa374819a19b582
Pinta365 <hello@pinta.land>
2023-10-11 12:54:37 +0200

    smallstats

--- src/smallstats.ts
+++ src/smallstats.ts
@@ -5,12 +5,14 @@ async function countEvents(entries) {
     let pageLoads = 0;
     let pageClicks = 0;
     let pageScrolls = 0;
+    let uniqueDeviceIds = new Set();
 
     for await (const entry of entries) {
         const event = entry.value;
         switch (event.type) {
             case "pageLoad":
                 pageLoads++;
+                uniqueDeviceIds.add(event.deviceId); 
                 break;
             case "pageClick":
                 pageClicks++;
@@ -20,10 +22,12 @@ async function countEvents(entries) {
                 break;
         }
     }
-    return [pageLoads, pageClicks, pageScrolls];
+    return {pageLoads, pageClicks, pageScrolls,  uniqueDevices: uniqueDeviceIds.size};
 }
 
 export async function smallStats(project) {
+    const now = new Date();
+    const thirtyMinutesAgo = new Date(now.getTime() - 30 * 60 * 1000);
     const today = new Date();
     const startOfToday = new Date(today.getFullYear(), today.getMonth(), today.getDate());
     const startOfYesterday = new Date(startOfToday - 24 * 60 * 60 * 1000);
@@ -41,8 +45,17 @@ export async function smallStats(project) {
     });
     const todaysEvents = await countEvents(todayEntries);
 
+    const last30MinEntries = database.list({
+        start: [project.id, thirtyMinutesAgo.getTime()],
+        end: [project.id, now.getTime()],
+    });
+    const last30MinEvents = await countEvents(last30MinEntries);
+
+
     const stats = `${project.name}
-    YESTERDAY: Page Loads: ${yesterdaysEvents[0]}, Page Clicks: ${yesterdaysEvents[1]}, Page Scrolls: ${yesterdaysEvents[2]}
-    TODAY: Page Loads: ${todaysEvents[0]}, Page Clicks: ${todaysEvents[1]}, Page Scrolls: ${todaysEvents[2]}`;
+    YESTERDAY: Page Loads: ${yesterdaysEvents.pageLoads}, Page Clicks: ${yesterdaysEvents.pageClicks}, Page Scrolls: ${yesterdaysEvents.pageScrolls}, Unique visitors: ${yesterdaysEvents.uniqueDevices}
+    TODAY: Page Loads: ${todaysEvents.pageLoads}, Page Clicks: ${todaysEvents.pageClicks}, Page Scrolls: ${todaysEvents.pageScrolls}, Unique visitors: ${todaysEvents.uniqueDevices}
+    LAST 30 MINUTES: Page Loads: ${last30MinEvents.pageLoads}, Page Clicks: ${last30MinEvents.pageClicks}, Page Scrolls: ${last30MinEvents.pageScrolls}, Unique visitors: ${last30MinEvents.uniqueDevices}`;
     return stats;
+    
 }
