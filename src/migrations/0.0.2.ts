9d1711cc6ca488bca76926d6a383e4c4e1f4c560
Hexagon <hexagon@56k.guru>
2023-10-22 13:01:48 +0000

    Missing code

--- src/migrations/0.0.2.ts
+++ src/migrations/0.0.2.ts
@@ -45,15 +45,26 @@ export async function migration(database: Deno.Kv) {
                 await database.set([loggerData.payloadId as string], loggerData);
 
             }
-
         }
 
         // Convert indices written during the old era
         const all4 = database.list({start: ["0"], end: ["Z"]});
         for await (const entry of all4) {
-            if (entry.value && Object.prototype.hasOwnProperty.call(entry.value,"type")) {
+            // Old entry
+            if (entry.value && Object.prototype.hasOwnProperty.call(entry.value,"payload") && Object.prototype.hasOwnProperty.call(entry.value,"type")) {
+                const loggerData = entry.value as OldLoggerData;
+                const converted: LoggerData = {
+                    ...(loggerData.payload as LoggerData)
+                };
+                if (!converted.type) converted.type = loggerData.type;
+                // Ensure all indices
+                await database.set([converted.projectId, converted.timestamp], converted);
+                await database.set([converted.projectId, loggerData.type, converted.timestamp], converted);
+                await database.set([converted.payloadId as string], converted);
+            // Newer entry
+            } else if (Object.prototype.hasOwnProperty.call(entry.value,"type")) {
                 const loggerData = entry.value as LoggerData;
-                
+
                 // Ensure all indices
                 await database.set([loggerData.projectId, loggerData.timestamp], loggerData);
                 await database.set([loggerData.projectId, loggerData.type, loggerData.timestamp], loggerData);
